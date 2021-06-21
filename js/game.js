d4f21421b17a5efff0db2c95b0b32d9a9373b7af
aurelie-castro <acdemarly@gmail.com>
2021-06-21 10:11:08 +0200

    erased commented out code

--- js/game.js
+++ js/game.js
@@ -72,7 +72,8 @@ function preload() {
 
 }
 
-function create() {    
+function create() {  
+    game.sound.mute = true;
     gameBg = this.add.image(180, 320, 'gameBg');
     
     var image = this.add.image(200, 300, 'background');
@@ -174,30 +175,10 @@ function create() {
     var zone9 = this.add.zone(245, 285, 30, 70).setRectangleDropZone(30, 70);
     zone9.setName('shoulderR');
     
-    //  Just a visual display of the drop zone
-//    var graphics = this.add.graphics();
-//    graphics.lineStyle(2, 0xffff00);
-//    graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
-//    
-//    graphics.strokeRect(zone2.x - zone2.input.hitArea.width / 2, zone2.y - zone2.input.hitArea.height / 2, zone2.input.hitArea.width, zone2.input.hitArea.height);
-//    
-//    graphics.strokeRect(zone3.x - zone3.input.hitArea.width / 2, zone3.y - zone3.input.hitArea.height / 2, zone3.input.hitArea.width, zone3.input.hitArea.height);
-//    
-//    graphics.strokeRect(zone4.x - zone4.input.hitArea.width / 2, zone4.y - zone4.input.hitArea.height / 2, zone4.input.hitArea.width, zone4.input.hitArea.height);
-//    
-//    graphics.strokeRect(zone5.x - zone5.input.hitArea.width / 2, zone5.y - zone5.input.hitArea.height / 2, zone5.input.hitArea.width, zone5.input.hitArea.height);
-//    
-//    graphics.strokeRect(zone6.x - zone6.input.hitArea.width / 2, zone6.y - zone6.input.hitArea.height / 2, zone6.input.hitArea.width, zone6.input.hitArea.height);
-//    
-//    graphics.strokeRect(zone7.x - zone7.input.hitArea.width / 2, zone7.y - zone7.input.hitArea.height / 2, zone7.input.hitArea.width, zone7.input.hitArea.height);
-//    
-//    graphics.strokeRect(zone8.x - zone8.input.hitArea.width / 2, zone8.y - zone8.input.hitArea.height / 2, zone8.input.hitArea.width, zone8.input.hitArea.height);
-//    
-//    graphics.strokeRect(zone9.x - zone9.input.hitArea.width / 2, zone9.y - zone9.input.hitArea.height / 2, zone9.input.hitArea.width, zone9.input.hitArea.height);
- 
     
     //---drag and drop mechanics---
      this.input.on('dragstart', function (pointer, gameObject) {
+         game.sound.mute = false;
          holdSound.play();
         this.children.bringToTop(gameObject);
     }, this);
@@ -211,17 +192,10 @@ function create() {
 
     this.input.on('dragenter', function (pointer, gameObject, dropZone) {
 
-//        graphics.clear();
-//        graphics.lineStyle(2, 0x00ffff);
-//        graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
-//        console.log(gameObject.name);
 
     });
 
     this.input.on('dragleave', function (pointer, gameObject, dropZone) {
-//            graphics.clear();
-//        graphics.lineStyle(2, 0xffff00);
-//        graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
 
     });
 
